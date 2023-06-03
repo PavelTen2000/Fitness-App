f0bfbb066d86530badfced42148e87ed3060fe08
Pavel <paveltenfs@gmail.com>
2023-06-03 23:53:57 +0600

    Added navbar

--- Gym-typescript/src/App.tsx
+++ Gym-typescript/src/App.tsx
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import NavBar from "./components/NavBar/NavBar";
 const App = () => {
+  const [selectedPage, setSelectedPage] = useState<string>("home");
   return (
     <div>
-      <NavBar />
+      <NavBar selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+      <div id="ourclasses"></div>
+      <div id="home"></div>
     </div>
   );
 };
